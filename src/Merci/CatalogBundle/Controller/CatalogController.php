f6c3c68f15cfcd2c159afb4bb564eb445a2f057a
Aluno <aluno@season.com.br>
2013-09-30 22:55:09 -0300

    Buscando dados no Banco

--- src/Merci/CatalogBundle/Controller/CatalogController.php
+++ src/Merci/CatalogBundle/Controller/CatalogController.php
@@ -8,11 +8,33 @@ class CatalogController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('MerciCatalogBundle:Catalog:index.html.twig');
+        $products = $this->getDoctrine()
+            ->getRepository('MerciCatalogBundle:Product')
+            ->findAll();
+
+        if (empty($products)) {
+            throw $this->createNotFoundException('No products avaiable');
+        }
+
+        return $this->render(
+            'MerciCatalogBundle:Catalog:index.html.twig',
+            array('products' => $products)
+        );
     }
 
-    public function productAction()
+    public function productAction($id)
     {
-        return $this->render('MerciCatalogBundle:Catalog:product.html.twig');
+        $product = $this->getDoctrine()
+            ->getRepository('MerciCatalogBundle:Product')
+            ->findOneById($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('No product found with id: ' . $id);
+        }
+
+        return $this->render(
+            'MerciCatalogBundle:Catalog:product.html.twig',
+            array('product' => $product)
+        );
     }
 }
