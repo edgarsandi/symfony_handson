7a45f7b2fb0c77b0bb342a9d05835b521f858343
Aluno <aluno@season.com.br>
2013-10-06 12:30:41 -0300

    Adicionando produto no Carrinho

--- src/Merci/CartBundle/Controller/CartController.php
+++ src/Merci/CartBundle/Controller/CartController.php
@@ -3,6 +3,7 @@
 namespace Merci\CartBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Merci\CartBundle\Entity\Cart;
 
 class CartController extends Controller
 {
@@ -13,6 +14,19 @@ class CartController extends Controller
 
     public function addAction($id)
     {
+        $product = $this->getDoctrine()
+            ->getRepository('MerciCatalogBundle:Product')
+            ->findOneById($id);
+
+        if (!$product) {
+            return $this->redirectAndNotify('Produto não existe');
+        }
+
+        $session = $this->getRequest()->getSession();
+        $cart = $session->get('cart', new Cart());
+        $cart->add($product);
+        $session->set('cart', $cart);
+
         return $this->render('MerciCartBundle:Default:index.html.twig');
     }
 
