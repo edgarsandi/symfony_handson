58151e01fb1b9a9aee1eacf61f0ebc0f5f32c8e3
Aluno <aluno@season.com.br>
2013-10-09 07:39:03 -0300

    Restringindo acesso usuario autenticado

--- src/Merci/UserBundle/Controller/UserController.php
+++ src/Merci/UserBundle/Controller/UserController.php
@@ -3,11 +3,36 @@
 namespace Merci\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class UserController extends Controller
 {
     public function loginAction()
     {
-        return $this->render('MerciUserBundle:Default:login.html.twig');
+        if ($this->get('security.context')->isGranted('ROLE_USER')) {
+            return $this->redirect($this->generateUrl('checkout'));
+        }
+
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render(
+            'MerciUserBundle:Default:login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+                'error'         => $error,
+            )
+        );
     }
 }
