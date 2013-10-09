0462c65bf5149bf7c49b6efaa972d7beccdabc8b
Aluno <aluno@season.com.br>
2013-10-09 16:33:07 -0300

    Send e-mail

--- src/Merci/CheckoutBundle/Controller/CheckoutController.php
+++ src/Merci/CheckoutBundle/Controller/CheckoutController.php
@@ -8,6 +8,40 @@ class CheckoutController extends Controller
 {
     public function indexAction()
     {
+        $session = $this->getRequest()->getSession();
+        $cart = $session->get('cart');
+        if (!$cart || $cart->count() == 0) {
+            return $this->redirect($this->generateUrl('homepage'));
+        }
+
         return $this->render('MerciCheckoutBundle:Default:index.html.twig');
+    }
+
+    public function successAction()
+    {
+        $session = $this->getRequest()->getSession();
+        $cart = $session->get('cart');
+        if (!$cart || $cart->count() == 0) {
+            return $this->redirect($this->generateUrl('homepage'));
+        }
+
+        $user = $this->get('security.context')->getToken()->getUser();
+        $session = $this->getRequest()->getSession();
+        $cart = $session->get('cart');
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Merci Commerce: Pedido realizado com sucesso!')
+            ->setFrom('atendimento@merci.com')
+            ->setTo($user->getEmail())
+            ->setBody(
+                $this->renderView(
+                    'MerciCheckoutBundle:Default:email.txt.twig',
+                    array('user' => $user, 'cart' => $cart)
+                ));
+        $this->get('mailer')->send($message);
+
+        $session->remove('cart');
+
+        return $this->render('MerciCheckoutBundle:Default:success.html.twig');
     }
 }
